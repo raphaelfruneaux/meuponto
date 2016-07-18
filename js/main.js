02fde7ec012e70d138d3bc575ee753b7c28e0987
U-MESTRE0\charleslennon <charleslennon@IE-TI-CHARLES.MESTRE.IEDUCAR>
2016-07-18 19:33:54 -0300

    limpando ponto após submit e adicionando em um array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var myApp = angular.module('myApp',['ui.mask']);
 
 myApp.controller('CounterController', ['$scope', function($scope) {
-  $scope.teste = function() {
-  	console.log("OPAAA");
+	$scope.ponto = '';
+	$scope.pontos = [];
+  $scope.addPonto = function() {
+  	console.log($scope.ponto);
+  	$scope.pontos.push($scope.ponto);
+  	console.log($scope.pontos);
+  	$scope.ponto = '';
   }
 }]);
