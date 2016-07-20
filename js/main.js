73ff7c380767b913482aacfb7216e1097a5e24c9
Raphael Fruneaux <raphaelfruno@gmail.com>
2016-07-20 19:17:29 -0300

    removendo comentários

--- js/main.js
+++ js/main.js
@@ -1,72 +1,61 @@
 
-var myApp = angular.module('myApp',['ui.mask']);
+	var myApp = angular.module('myApp',['ui.mask']);
 
-myApp.controller('CounterController', ['$scope', '$filter', function($scope, $filter) {
-	// {
-  //   "user": {
-  //       "id": 1,
-  //       "nome": "Fulano de Tal",
-  //       "email": "emaildefulano@tenta.ai",
-  //       "registros": [{
-  //           "dia": "2016-07-20",
-  //           "pontos": []
-  //       },]
-  //   },
-	// }
-
-	var dataStorage = JSON.parse(localStorage.getItem("pontoEletronico"));
-	if (!dataStorage) {
-			var pontoEletronico = {user: {}};
-			pontoEletronico.user.name = prompt("Informe o seu nome:");
-			pontoEletronico.user.email = prompt("Informe o seu email:");
-			pontoEletronico.user.registros = [];
-			localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
-	} else {
-		var pontoEletronico = dataStorage;
-	}
-
-	var today = new Date().toISOString().match(/\d{4}-\d{2}-\d{2}/).join('-');
-	var current = $filter('filter')(pontoEletronico.user.registros, {date: today})[0];
-
-	if (current.length < 1) {
-		var registro = {
-			date: today,
-			pontos: []
-		};
-		current = registro;
-		pontoEletronico.user.registros.push(registro);
-		localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
-	}
-
-	$scope.ponto = '';
-	$scope.pontos = current.pontos;
-
-	$scope.addPonto = function () {
-  	if ($scope.ponto) {
-	  	$scope.pontos.push(formatPonto(angular.copy($scope.ponto)));
-			$scope.ponto = '';
-			localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
-  	}
-  };
-
-	$scope.horasTrabalhadas = function () {
-		var diffs = [];
-
-		for (i in $scope.pontos) {
-			if (i % 2 != 0) {
-				diffs.push(hmh.diff(toHMH($scope.pontos[i-1]), toHMH($scope.pontos[i])).toString().replace(/\s+/g, ''));
-			}
+	myApp.controller('CounterController', ['$scope', '$filter', function($scope, $filter) {
+		
+		var dataStorage = JSON.parse(localStorage.getItem("pontoEletronico"));
+		if (!dataStorage) {
+				var pontoEletronico = {user: {}};
+				pontoEletronico.user.name = prompt("Informe o seu nome:");
+				pontoEletronico.user.email = prompt("Informe o seu email:");
+				pontoEletronico.user.registros = [];
+				localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
+		} else {
+			var pontoEletronico = dataStorage;
 		}
 
-		return hmh.sum(diffs).toString() || 0;
-	};
+		var today = new Date().toISOString().match(/\d{4}-\d{2}-\d{2}/).join('-');
+		var current = $filter('filter')(pontoEletronico.user.registros, {date: today})[0];
 
-	function formatPonto (p) {
-  	return p.charAt(0) + p.charAt(1) + ":" + p.charAt(2) + p.charAt(3);
-  }
+		if (current.length < 1) {
+			var registro = {
+				date: today,
+				pontos: []
+			};
+			current = registro;
+			pontoEletronico.user.registros.push(registro);
+			localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
+		}
 
-	function toHMH (p) {
-		p = p.match(/\d+/g).join('');
-		return p.charAt(0) + p.charAt(1) + "h" + p.charAt(2) + p.charAt(3) + "m";
-	}
-}]);
+		$scope.ponto = '';
+		$scope.pontos = current.pontos;
+
+		$scope.addPonto = function () {
+	  	if ($scope.ponto) {
+		  	$scope.pontos.push(formatPonto(angular.copy($scope.ponto)));
+				$scope.ponto = '';
+				localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
+	  	}
+	  };
+
+		$scope.horasTrabalhadas = function () {
+			var diffs = [];
+
+			for (i in $scope.pontos) {
+				if (i % 2 != 0) {
+					diffs.push(hmh.diff(toHMH($scope.pontos[i-1]), toHMH($scope.pontos[i])).toString().replace(/\s+/g, ''));
+				}
+			}
+
+			return hmh.sum(diffs).toString() || 0;
+		};
+
+		function formatPonto (p) {
+	  	return p.charAt(0) + p.charAt(1) + ":" + p.charAt(2) + p.charAt(3);
+	  }
+
+		function toHMH (p) {
+			p = p.match(/\d+/g).join('');
+			return p.charAt(0) + p.charAt(1) + "h" + p.charAt(2) + p.charAt(3) + "m";
+		}
+	}]);
