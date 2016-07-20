aa918ac982b43a9a1c18e4ad390b540a4417aa56
Raphael Fruneaux <raphaelfruno@gmail.com>
2016-07-20 19:15:19 -0300

    armazenamento localStorage

--- js/main.js
+++ js/main.js
@@ -1,16 +1,51 @@
 
 var myApp = angular.module('myApp',['ui.mask']);
 
-myApp.controller('CounterController', ['$scope', function($scope) {
-	console.log(hmh);
-	
+myApp.controller('CounterController', ['$scope', '$filter', function($scope, $filter) {
+	// {
+  //   "user": {
+  //       "id": 1,
+  //       "nome": "Fulano de Tal",
+  //       "email": "emaildefulano@tenta.ai",
+  //       "registros": [{
+  //           "dia": "2016-07-20",
+  //           "pontos": []
+  //       },]
+  //   },
+	// }
+
+	var dataStorage = JSON.parse(localStorage.getItem("pontoEletronico"));
+	if (!dataStorage) {
+			var pontoEletronico = {user: {}};
+			pontoEletronico.user.name = prompt("Informe o seu nome:");
+			pontoEletronico.user.email = prompt("Informe o seu email:");
+			pontoEletronico.user.registros = [];
+			localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
+	} else {
+		var pontoEletronico = dataStorage;
+	}
+
+	var today = new Date().toISOString().match(/\d{4}-\d{2}-\d{2}/).join('-');
+	var current = $filter('filter')(pontoEletronico.user.registros, {date: today})[0];
+
+	if (current.length < 1) {
+		var registro = {
+			date: today,
+			pontos: []
+		};
+		current = registro;
+		pontoEletronico.user.registros.push(registro);
+		localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
+	}
+
 	$scope.ponto = '';
-	$scope.pontos = [];
+	$scope.pontos = current.pontos;
 
 	$scope.addPonto = function () {
   	if ($scope.ponto) {
 	  	$scope.pontos.push(formatPonto(angular.copy($scope.ponto)));
 			$scope.ponto = '';
+			localStorage.setItem("pontoEletronico", JSON.stringify(pontoEletronico));
   	}
   };
 
